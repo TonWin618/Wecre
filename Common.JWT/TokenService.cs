03914cf37225a15f054ab971b09f962e96334f72
TonWin <tonwin618@qq.com>
2023-02-19 23:35:10 +0800

    fix bugs

--- Common.JWT/TokenService.cs
+++ Common.JWT/TokenService.cs
@@ -10,7 +10,7 @@ public class TokenService : ITokenService
     public string BuildToken(IEnumerable<Claim> claims, JWTOptions options)
     {
         TimeSpan expiryDuration = TimeSpan.FromSeconds(options.ExpireSeconds);
-        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Key));
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("dnbsauihduibhcjbiugwydvwgdkhjn"));
         var credentials = new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256Signature);
         var tokenDescriptor = new JwtSecurityToken(options.Issuer, options.Audience,
             claims, expires: DateTime.Now.Add(expiryDuration), signingCredentials: credentials);

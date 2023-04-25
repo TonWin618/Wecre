f8d5194418d30acadb086a96083bb543e3626e6b
tonwin618 <tonwin618@gmail.com>
2023-04-25 21:46:34 +0800

    feat: Add firmwares page

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,7 +1,7 @@
 <template>
     <a-layout>
-        <a-layout-header>
-            <div class="header-row1" style="width: 100%;">
+        <a-layout-header class="header">
+            <div class="header-name">
                 <a-breadcrumb>
                     <a-breadcrumb-item class="user-name"><a :href="`/${$route.params.name}`">{{ $route.params.name
                     }}</a></a-breadcrumb-item>
@@ -9,7 +9,7 @@
                         $route.params.project }}</a></a-breadcrumb-item>
                 </a-breadcrumb>
             </div>
-            <div class="header-row2" style="width: 100%;">
+            <div class="header-menu">
                 <a-menu v-model:selectedKeys="current" mode="horizontal">
                     <a-menu-item key="publish">
                         <FileOutlined />
@@ -29,12 +29,11 @@
                     </a-menu-item>
                 </a-menu>
             </div>
-            <div class="header-version-form" style="">
+            <div class="header-version">
                 <a-space>
                 <span style="font-size: 18px;color: black;">version</span>
                 <a-select v-model:value="value" show-search placeholder="Select a version" style="width: 200px"
-                    :options="options" :filter-option="filterOption" @focus="handleFocus" @blur="handleBlur"
-                    @change="handleChange"></a-select>
+                    :options="options" :filter-option="filterOption" ></a-select>
                 <a @click='ShowHistory'>
                     <HistoryOutlined style="color: black;"/>
                 </a>
@@ -47,13 +46,14 @@
                 <Replay v-if="current[0] === 'comments'" />
                 <Comments v-if="current[0] === 'comments'" />
                 <History v-if="current[0] === 'history'"/>
+                <FirmwareList v-if="current[0]=== 'firmwares'" />
             </div>
         </a-layout-content>
     </a-layout>
 </template>
     
-<script setup lang="ts" scoped>
-import { CodepenOutlined, StarOutlined, FileOutlined, FileZipOutlined, CommentOutlined, HistoryOutlined } from '@ant-design/icons-vue';
+<script setup lang="ts">
+import { CodepenOutlined, FileOutlined, FileZipOutlined, CommentOutlined, HistoryOutlined } from '@ant-design/icons-vue';
 import { ref } from 'vue';
 import type { SelectProps } from 'ant-design-vue';
 
@@ -61,31 +61,36 @@ import Comments from '@/components/Project/Comments/Comments.vue';
 import Replay from '@/components/Project/Comments/Replay.vue';
 import History from '@/components/Project/History/History.vue';
 import PublishContainer from '@/containers/PublishContainer.vue';
+import FirmwareList from '@/components/Project/Firmwares/FirmwareList.vue';
 
 function ShowHistory(){
     current.value[0] = 'history'
 }
+
 const current = ref<string[]>(['publish']);
+
 const options = ref<SelectProps['options']>([
     { value: 'V1.01', label: 'V1.01' },
     { value: 'V1.01 Alpha', label: 'V1.01 Alpha' },
     { value: 'V1.00', label: 'V1.00' },
 ]);
+
 const value = ref();
-const handleChange = (value: string) => {
-    console.log(`selected ${value}`);
-};
-const handleBlur = () => {
-    console.log('blur');
-};
-const handleFocus = () => {
-    console.log('focus');
-};
 const filterOption = (input: string, option: any) => {
     return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
 };
 </script>
+
 <style lang="less" scoped>
+.header-name{
+    padding-right: 80px;
+}
+.header-menu{
+    max-width: 600px;
+}
+.header-version{
+    margin-left: auto;
+}
 .user-name a {
     font-size: 20px;
     font-style: normal;
@@ -96,16 +101,6 @@ const filterOption = (input: string, option: any) => {
     font-size: 20px;
     font-weight: bolder;
     color: @link-color !important;
-}
-.header-version-form{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    margin: 0 0;
-    padding-left: 200px;
-}
-.header-version-form .ant-select:not(.ant-select-customize-input) .ant-select-selector{
-    background-color: white;
 }
 .ant-layout {
     width: 100%;
@@ -118,13 +113,6 @@ const filterOption = (input: string, option: any) => {
     border-bottom: 1px solid @border-color-base;
     background-color: @component-background;
     height: 65px;
-}
-
-.ant-layout-sider {
-    height: 100%;
-    padding: 10px;
-    text-align: center;
-    background-color: red;
 }
 
 .ant-layout-content {
